fc357169e9880706f2d594d3eec44528e5fd352f
Yoon-Jiyeon <she79560@naver.com>
2022-09-06 23:25:11 +0900

    [FEAT] HTML 파일 "간단하게" 만드는 기능 추가 (-지연)

--- Assets/Scripts/DragAndDrop/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop/DragAndDrop.cs
@@ -5,17 +5,9 @@ using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
-    //[SerializeField] GameObject cardImagePrefab;
-    //GameObject devCard;
     public static Vector2 DefaultPos;
     RectTransform rectTransform;
     Canvas devCanvas;
-    //[SerializeField] GameObject devArea;
-
-    private void Awake() 
-    {
-        //rectTransform = this.gameObject.GetComponent<RectTransform>();
-    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -26,17 +18,11 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
 
     public void OnDrag(PointerEventData eventData)
     {
-        // 이전 이동과 비교해서 얼마나 이동했는지 보여줌
-        // 캔버스의 스케일과 맞춰야하므로
         rectTransform.anchoredPosition += eventData.delta / devCanvas.scaleFactor;
-        //Vector2 currentPos = eventData.position;
-        //this.transform.position = currentPos;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-       // this.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0f);
     }
 
     public void OnPointerDown(PointerEventData eventData)
